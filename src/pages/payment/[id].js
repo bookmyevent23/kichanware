73de2fb52cb45c110a260f4201b386b2f2d08794
bookmyevent23 <158741481+bookmyevent23@users.noreply.github.com>
2024-11-08 12:20:49 +0530

    Update [id].js

--- src/pages/payment/[id].js
+++ src/pages/payment/[id].js
@@ -89,26 +89,31 @@ const Payments = () => {
             return;
         }
 
-        const supportedInstruments = [
-            {
-                supportedMethods: ["https://tez.google.com/pay"],
-                data: {
-                    pa: products.upi, // Replace with your Merchant UPI ID
-                    pn: "Merchant Name", // Replace with your Merchant Name
-                    tr: "1234ABCD", // Your custom transaction reference ID
-                    url: "https://yourwebsite.com/order/1234ABCD", // URL of the order in your website
-                    mc: "1234", // Your merchant category code
-                    tn: "Purchase in Merchant", // Transaction note
-                },
-            },
-        ];
+      
+  const supportedInstruments = [
+    {
+      supportedMethods: ['https://tez.google.com/pay'],
+      data: {
+       pa: products.upi,
+        pn: 'Merchant Name',
+        tr: '1234ABCD',  // your custom transaction reference ID
+        url: 'http://url/of/the/order/in/your/website',
+        mc: '1234', // your merchant category code
+        tn: 'Purchase in Merchant',
+        gstBrkUp: 'GST:16.90|CGST:08.45|SGST:08.45', // GST value break up
+        invoiceNo: 'BillRef123', // your invoice number
+        invoiceDate: '2019-06-11T13:21:50+05:30', // your invoice date and time
+        gstIn: '29ABCDE1234F2Z5', // your GSTIN
+      },
+    }
+  ];
 
         const details = {
             total: {
                 label: "Total",
                 amount: {
                     currency: "INR",
-                    value: router.query.id?.split('.')[0], // Amount to be paid
+                    value: 1, // Amount to be paid
                 },
             },
             displayItems: [
@@ -116,7 +121,7 @@ const Payments = () => {
                     label: "Original Amount",
                     amount: {
                         currency: "INR",
-                        value: router.query.id?.split('.')[0],
+                        value: 1,
                     },
                 },
             ],
