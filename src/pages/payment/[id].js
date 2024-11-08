f5893936dc8ea80df6b9dc05ab1e0ae6d46dc50f
bookmyevent23 <158741481+bookmyevent23@users.noreply.github.com>
2024-11-08 12:22:24 +0530

    Update [id].js

--- src/pages/payment/[id].js
+++ src/pages/payment/[id].js
@@ -92,7 +92,7 @@ const Payments = () => {
       
   const supportedInstruments = [
     {
-      supportedMethods: ['https://tez.google.com/pay'],
+      supportedMethods: ['https://gpay.google.com/pay'],
       data: {
        pa: products.upi,
         pn: 'Merchant Name',
